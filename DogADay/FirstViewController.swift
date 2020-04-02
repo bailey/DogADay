663a3c346a4001c7ca7f56966e78be70cee23097
Bailey Brooks <baileybrooks@me.com>
2020-04-01 18:20:52 -0700

    Initial Commit

---
+++ DogADay/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DogADay
+//
+//  Created by Bailey Brooks on 4/1/20.
+//  Copyright © 2020 Bailey Brooks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DogADay/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DogADay
+//
+//  Created by Bailey Brooks on 4/1/20.
+//  Copyright © 2020 Bailey Brooks. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
