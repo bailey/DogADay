d8444e8dfe5a6d36ea53377b46c4a72dc389d560
Bailey Brooks <baileybrooks@me.com>
2020-04-03 23:06:57 -0700

    camera changes - current state: loops at camera cancel attempt

--- DogADay/CameraViewController.swift
+++ DogADay/CameraViewController.swift
@@ -14,19 +14,24 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+        
+        
+        
     }
     
 }
 
 extension CameraViewController : UIImagePickerControllerDelegate {
-    @IBAction func takePhoto(_ sender: Any) {
-        imagePicker =  UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .camera
-
-        present(imagePicker, animated: true, completion: nil)
-    }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imagePicker.dismiss(animated: true, completion: nil)
@@ -34,3 +39,60 @@ extension CameraViewController : UIImagePickerControllerDelegate {
     }
 }
 
+//import UIKit
+//import AVFoundation
+//import Foundation
+//
+//class CameraViewController: UIViewController, UINavigationControllerDelegate {
+//    @IBOutlet weak var imgOverlay: UIImageView!
+//    @IBOutlet weak var navigationBar: UINavigationBar!
+//    @IBOutlet weak var btnCapture: UIButton!
+//
+//    @IBOutlet weak var shapeLayer: UIView!
+//
+//    let captureSession = AVCaptureSession()
+//    let stillImageOutput = AVCapturePhotoOutput()
+//    var previewLayer : AVCaptureVideoPreviewLayer?
+//
+//    var captureDevice : AVCaptureDevice?
+//
+//    // var imagePicker: UIImagePickerController!
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+//
+//}
+//
+//extension UIImage {
+//    convenience init(view: UIView) {
+//        UIGraphicsBeginImageContext(view.frame.size)
+//        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+//        let image = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        self.init(cgImage: (image?.cgImage)!)
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//extension CameraViewController : UIImagePickerControllerDelegate {
+//    @IBAction func takePhoto(_ sender: Any) {
+//        imagePicker =  UIImagePickerController()
+//        imagePicker.delegate = self
+//        imagePicker.sourceType = .camera
+//
+//        present(imagePicker, animated: true, completion: nil)
+//    }
+//
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        imagePicker.dismiss(animated: true, completion: nil)
+//        imageView.image = info[.originalImage] as? UIImage
+//    }
+//}
+//
