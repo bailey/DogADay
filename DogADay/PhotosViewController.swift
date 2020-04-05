1ea9abf88d857b8238e77b35df2fc6ed5d8dbbb5
Bailey Brooks <baileybrooks@me.com>
2020-04-04 22:40:01 -0700

    add app icon + changes to create video button

--- DogADay/PhotosViewController.swift
+++ DogADay/PhotosViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 // MARK: - ImageCell
 class ImageCell: UICollectionViewCell {
     @IBOutlet weak var newImageView: UIImageView!
+    @IBOutlet weak var photoDateView: UITextField!
 }
 
 // MARK: - PhotosViewController
@@ -123,3 +124,4 @@ extension PhotosViewController {
 //        https://stackoverflow.com/questions/40883784/build-video-from-uiimage-using-swift
     }
 }
+
